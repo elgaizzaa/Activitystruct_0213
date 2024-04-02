592bbe852d96abeaf82dd444aeea7199d4e49234
elgaizzaa <161436093+elgaizzaa@users.noreply.github.com>
2024-04-02 14:01:57 +0700

    Add project files.

---
+++ 213_struct/213_struct.cpp
@@ -0,0 +1,18 @@
+// 213_struct.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+struct Mahasiswa {
+    string nim;
+    string nama;
+    string alamat;
+    int umur;
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
